412a645105a9b067cf21e712cfd0417845c4806d
root <root@localhost.localdomain>
2020-02-08 00:44:33 -0500

    added install wget

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x *.sh && sh set_static_ip.sh && sh set_vpn_env.sh && sh start_vpn.sh && start_ss_server.sh
+yum install -y wget && chmod +x *.sh && sh set_static_ip.sh && sh set_vpn_env.sh && sh start_vpn.sh && start_ss_server.sh
